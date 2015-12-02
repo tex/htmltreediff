438ffc5e76dd06445fea5e685713ffac9d28c0b1
Kyle Gibson <kyle.gibson@frozenonline.com>
2015-12-02 00:01:59 -0500

    Try updating the test case

--- htmltreediff/tests.py
+++ htmltreediff/tests.py
@@ -120,9 +120,9 @@ test_cases = [  # test case = (old html, new html, inline changes, edit script)
     ),
     (
         'simple insert with tail text',
-        '<div>tail</div>',
-        '<div><h1>one</h1>tail</div>',
-        '<div><ins><h1>one</h1></ins>tail</div>',
+        '<p>tail</p>',
+        '<p><h1>one</h1>tail</p>',
+        '<p><ins><h1>one</h1></ins>tail</p>',
         [
             ('insert', [0, 0], {
                 'node_type': Node.ELEMENT_NODE,
