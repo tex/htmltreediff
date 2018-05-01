4673f8776adbfca6ae57214b406e5ff67e4579cd
Milan Svoboda <milan.svoboda@centrum.cz>
2018-05-01 23:02:53 +0200

    utf-8 cli, no strict requirements

--- htmltreediff/cli.py
+++ htmltreediff/cli.py
@@ -1,13 +1,14 @@
+import codecs
 import sys
-from htmltreediff import diff
 
+from htmltreediff import diff
 
 def main(argv=None):
     if not argv:
         argv = sys.argv  # pragma: no cover
-    with open(argv[1]) as file_a:
+    with codecs.open(argv[1], 'r', 'utf-8') as file_a:
         html_a = file_a.read()
-    with open(argv[2]) as file_b:
+    with codecs.open(argv[2], 'r', 'utf-8') as file_b:
         html_b = file_b.read()
     print diff(html_a, html_b, cutoff=0.0, pretty=True)
 
